90c3b4557ada926d7bbde8dc5bc986f5ee236e9f
Aldoforce <aldo@aldofernandez.com>
2014-06-02 22:07:34 -0300

    - retrieve and post data from/to salesforce

--- web.rb
+++ web.rb
@@ -9,46 +9,69 @@ $LOAD_PATH.unshift( File.join( File.dirname(__FILE__), 'lib' ) )
 require 'Salesforce_Connector'
 
 class App < Sinatra::Base
-    register Sinatra::Twitter::Bootstrap::Assets
+  register Sinatra::Twitter::Bootstrap::Assets
 
-    get '/' do
-      slim :apply
+  get '/' do
+    slim :apply
+  end
+
+  get '/test' do
+    sfdc = Salesforce_Connector.new
+    sfdc.test
+  end
+
+  get '/apply' do
+    slim :apply
+  end
+
+  post '/create_candidate' do
+    #create sfdc connector
+  	sfdc = Salesforce_Connector.new
+
+    #create candidate
+  	id = sfdc.create_candidate(
+  		params[:email],
+  		params[:location],
+  		params[:apply_as]
+  	)
+
+    if not(id) #user already exists
+      @message  = 'user already exists'
+      @info    = params[:email]
+      slim :message
+    else    
+      #encode id
+      encoded_id = Base64.encode64(id)
+
+      #redirect to details page
+      redirect '/candidate_details/' + encoded_id
     end
 
-    get '/test' do
-      sfdc = Salesforce_Connector.new
-      sfdc.test
-    end
+  end
 
-    get '/apply' do
-      slim :apply
-    end
+  get '/candidate_details/:encoded_id' do
+    #create sfdc connector
+    sfdc = Salesforce_Connector.new
+    @candidate = sfdc.get_candidate( 
+      params[:encoded_id]
+    )
 
-    post '/create_candidate' do
-    	sfdc = Salesforce_Connector.new
-    	id = sfdc.create_candidate(
-    		params[:email],
-    		params[:location],
-    		params[:apply_as]
-    	)
+    @encoded_id = params[:encoded_id]
+    
+    slim :candidate_details
+  end
 
-    	#encode id
-    	encoded_id = Base64.encode64(id)
-		
-			redirect '/candidate_details/' + encoded_id
+  post '/candidate_update' do
+    #create sfdc connector
+    sfdc = Salesforce_Connector.new
 
-    end
+    #pass the bindings
+    success = sfdc.update_candidate(params)
 
-    get '/candidate_details/:encoded_id' do
-      @encoded_id = :encoded_id
-      slim :candidate_details
-    end
-
-    get '/login' do
-    	slim :login
-    end
-
-    get '/signup' do
-    	slim :signup
-    end
+    @success = "Record updated" if success 
+    @message = "Update failed" if !success
+    @info = params if !success
+    
+    slim :message
+  end
 end
